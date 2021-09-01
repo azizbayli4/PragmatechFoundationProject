8eed45f7e68e4836ee3026e155fd6cd668768318
Cavad Azizbayli <cavad.azizbayli.8@gmail.com>
2021-09-01 12:59:35 +0400

    Task function "Foo"

--- Works/Week-04/jsPractise-02/app2.js
+++ Works/Week-04/jsPractise-02/app2.js
@@ -1,10 +1,10 @@
-let a = 0;
-
-function Increase() {
-  console.log(a);
-  a++;
-  if (a <= 10) {
-    Increase();
+function Foo(...array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
   }
+  //return sum;
+  console.log(sum);
 }
-Increase();
+Foo(3, 4, 12, 45, 67, 78);
+//console.log(Foo(3, 4, 12, 45, 67, 78))
