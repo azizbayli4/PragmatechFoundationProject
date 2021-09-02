3cdd43be50762fa2a051f9011d5cdf7002ef2517
Cavad Azizbayli <cavad.azizbayli.8@gmail.com>
2021-09-02 10:28:53 +0400

    Tasks edited and added

--- Algorithms/JS_Task-01/app.js
+++ Algorithms/JS_Task-01/app.js
@@ -1,27 +1,39 @@
-//2. a=5; b=6 yenidən təyin etmeden ele edin ki c.log(a) yazanda 6 digersinde 5 cixsin
+//1) a=5; b=6; yenidən təyin etmeden ele edin ki c.log(a) yazanda 6 digerinde 5 cixsin
 
-// //my way
+//my way
 let a = 5;
 let b = 6;
-let c;
+let temp;
 
-c = a;
-a = b;
-b = c;
-
+console.log("Before the method:");
 console.log(a);
 console.log(b);
+
+temp = a;
+a = b;
+b = temp;
+
+console.log("After applying rechange method:");
+console.log(a);
+console.log(b);
+
+console.log("-----------------");
 
 //after research
 
 //first way
-// let a;
-// let b;
+let c = 5;
+let d = 6;
 
-// [a, b] = [b,a];
+console.log("Before the method:");
+console.log(c);
+console.log(d);
 
-// console.log(a);
-// console.log(b);
+[c, d] = [d, c];
+
+console.log("After applying rechange method:");
+console.log(c);
+console.log(d);
 
 //second way
 // let a = 1;
@@ -34,21 +46,28 @@ console.log(b);
 // console.log(a);
 // console.log(b);
 
-//a++ və ++a arasındakı fərq
+console.log("-----------------");
+
+//2) a++ və ++a arasındakı fərq nədir?
 //code example
 var x = 10;
 var y = 20;
+console.log(x);
+console.log(y);
+
 //pre-increment operator
 x = ++x;
-document.write("++x = " + x);
+console.log("++x = " + x);
 //post-increment operator
 y = y++;
-document.write("<br> y++ = " + y);
+console.log("y++ = " + y);
+
+console.log("But without initializing");
 
 var x1 = 10;
 x1++;
-document.write("<br>" + x1);
+console.log(x1);
 
 var y1 = 20;
 ++y1;
-document.write("<br>" + y1);
+console.log(y1);
