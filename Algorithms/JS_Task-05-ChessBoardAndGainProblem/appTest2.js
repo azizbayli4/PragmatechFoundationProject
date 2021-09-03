9a489ad66937ca1a9aa028a7a3a0226f4c241e63
Cavad Azizbayli <cavad.azizbayli.8@gmail.com>
2021-09-03 12:03:31 +0400

    Task edited for better understanding

--- Algorithms/JS_Task-05-ChessBoardAndGainProblem/appTest2.js
+++ Algorithms/JS_Task-05-ChessBoardAndGainProblem/appTest2.js
@@ -1,12 +1,13 @@
 let x = 1;
-let gWeight = 0.024;
-let total = x;
+let gWeight = 0.065;
+let total = 1;
 
 for (let i = 1; i < 64; i++) {
   x = x * 2;
   total += x;
 }
-console.log("Total:" + total);
-// let totalgWeight = total * gWeight;
-// let kgWeight = totalgWeight / 1000;
-// console.log(kgWeight + "kg");
+
+console.log(`Total grain amount is : ${total} `);
+let totalgWeight = total * gWeight;
+console.log(`Total amount (kg) is : ${totalgWeight / 1000} kg`);
+console.log(`Total amount (tonne) is : ${totalgWeight / 1000000} metric tons`);
