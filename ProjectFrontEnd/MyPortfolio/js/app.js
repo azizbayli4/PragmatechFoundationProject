82462184d2d60c9eae7d86d664429fcc1b90331e
Cavad Azizbayli <cavad.azizbayli.8@gmail.com>
2021-09-19 23:46:50 +0400

    Portfolio is ready. Need some fixes (slider should change and mobile menu needs minor additions

---
+++ ProjectFrontEnd/MyPortfolio/js/app.js
@@ -0,0 +1,21 @@
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+$(document).ready(function () {
+  $("#testimonial-slider").owlCarousel({
+    items: 1,
+    itemsDesktop: [1000, 1],
+    itemsDesktopSmall: [979, 1],
+    itemsTablet: [768, 1],
+    pagination: true,
+    navigation: true,
+    navigationText: ["", ""],
+    slideSpeed: 1000,
+    autoPlay: true,
+  });
+});
